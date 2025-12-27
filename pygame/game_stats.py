e2d10e3759afcf82e09f5172e372d13a4cf6e31f
José Gabriel <gabrijuvis@gmail.com>
2025-12-27 20:12:59 -0300

    pontuação máxima p1

--- pygame/game_stats.py
+++ pygame/game_stats.py
@@ -7,6 +7,9 @@ class GameStats():
         self.reset_stats()
         self.game_active = False 
 
+        # A pontuação máxima jamais deverá ser reiniciada
+        self.high_score = 0
+
     def reset_stats(self):
         """Inicializa os dados estatísticos que podem mudar durante o jogo."""
         self.ship_left = self.al_settings.ship_limit
--- pygame/scoreboard.py
+++ pygame/scoreboard.py
@@ -31,3 +31,14 @@ class Scoreboard():
     def show_score(self):
         """Desenha a pontuação na tela"""
         self.screen.blit(self.score_image, self.score_rect)
+
+    def prep_high_score(self):
+        """Transforma a pontuação máxima em uma imagem renderizada"""
+        high_score = int(round(self.stats.high_score, -1))
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.al_settings.bg_color)
+
+        # Centraliza a pontuação máxima na parte superior da tela
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.high_score_rect.top
