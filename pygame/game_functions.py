c23485a192bfc318bbfc4334f612598ac73e850d
José Gabriel <gabrijuvis@gmail.com>
2025-12-30 21:48:54 -0300

    Pontuação máxima p2

--- pygame/game_functions.py
+++ pygame/game_functions.py
@@ -113,7 +113,7 @@ def check_bullet_alien_collisions(al_settings, screen, stats, sb, ship, aliens, 
             stats.score += al_settings.alien_points
             sb.prep_score()
 
-
+        check_high_score(stats, sb)
 
 
 def fire_bullet(al_settings, screen, ship, bullets):
@@ -223,3 +223,9 @@ def update_aliens(al_settings, stats, screen, ship, aliens, bullets):
 
     # Verifica se há algum alienígena que atingiu a parte inferior da tela
     check_aliens_bottom(al_settings, stats, screen, ship, aliens, bullets)
+
+def check_high_score(stats, sb):
+    """Verifica se há uma nova pontuação máxima"""
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sb.prep_high_score()
--- pygame/scoreboard.py
+++ pygame/scoreboard.py
@@ -16,6 +16,7 @@ class Scoreboard():
 
         # Prepara a imagem da pontuação inicial
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """Transforma a pontuação em uma imagem renderizada"""
@@ -28,10 +29,6 @@ class Scoreboard():
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
-    def show_score(self):
-        """Desenha a pontuação na tela"""
-        self.screen.blit(self.score_image, self.score_rect)
-
     def prep_high_score(self):
         """Transforma a pontuação máxima em uma imagem renderizada"""
         high_score = int(round(self.stats.high_score, -1))
@@ -42,3 +39,10 @@ class Scoreboard():
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.high_score_rect.top
+    
+    def show_score(self):
+        """Desenha a pontuação na tela"""
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+
+    
