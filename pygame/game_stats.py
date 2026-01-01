fd4715ed1923552cf937fc62c7db234a32ad2d27
José Gabriel <gabrijuvis@gmail.com>
2025-12-31 22:13:19 -0300

    Adicionado mudança de nível

--- pygame/game_stats.py
+++ pygame/game_stats.py
@@ -14,3 +14,4 @@ class GameStats():
         """Inicializa os dados estatísticos que podem mudar durante o jogo."""
         self.ship_left = self.al_settings.ship_limit
         self.score = 0
+        self.level = 1
